0fc44eccb49f9f2272c8e32aad52dceb81a1abaa
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-16 10:45:14 +0200

    package

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -15,6 +15,9 @@ function main(numOne, numTwo){
     return quotient;
 }
 
-console.log(main(15, 3));  // change this line
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0)); // change this line
 
 module.exports = main;
