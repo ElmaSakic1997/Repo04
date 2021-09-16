268e3633e510985e42a0a828bd07922b8b72150e
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-16 10:26:36 +0200

    decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+
+    // only change code below this line
+    var myDecimal = 2.54;
+    var myDecimalE = myDecimal * 10e-4;
+    // Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+
+module.exports = main;
