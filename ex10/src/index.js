58692e458791ef5f416ec132cf2971232ec66c2e
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-16 13:33:24 +0200

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+    // only change code below this line
+    var myFirstName = "Elma";
+    var myLastName = "Sakic";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
